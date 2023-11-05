c35d2fc690d7697436f2180c0347a15e0255e62d
Hegedűs Júlia <hegedus.julia.erzsebet@gmail.com>
2023-11-05 12:25:32 +0100

    Initial commit

---
+++ src/main/java/com/chokie/springcucumberexample/SpringCucumberExampleApplication.java
@@ -0,0 +1,13 @@
+package com.chokie.springcucumberexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberExampleApplication.class, args);
+	}
+
+}
